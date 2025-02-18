543c664be4530f14da6e82898eabdb1a3a1a1696
Joacs49 <gaelmzl49@gmail.com>
2025-02-18 10:59:04 -0500

    Squashed commit of the following:
    commit 64b08247495b2339de1f0f8ba627e588ba21604f
    Author: Joacs49 <gaelmzl49@gmail.com>
    Date:   Wed Feb 12 11:59:28 2025 -0500
    
        Docker
    
    commit fe299dedb8ce97c5b58ead246e99b4d353c48a91
    Author: Joacs49 <gaelmzl49@gmail.com>
    Date:   Wed Feb 12 11:29:13 2025 -0500
    
        Fix: Se reparo conflictos
    
    commit 6c137c5347032493fafa7ce5992f457e673c4feb
    Merge: 9744fdb 4da34d1
    Author: Joacs49 <gaelmzl49@gmail.com>
    Date:   Wed Feb 12 11:28:47 2025 -0500
    
        Merge branch 'master' of https://github.com/yuntasproducciones/tami-back-teams
    
    commit 9744fdbd48f97bf03ed239d01db9234a52505a66
    Author: Joacs49 <gaelmzl49@gmail.com>
    Date:   Wed Feb 12 11:25:45 2025 -0500
    
        Se agrego el .env.example
    
    commit 4da34d1da1266b5fddc04c0f5aa4b808f93aabba
    Merge: a528725 18e8511
    Author: Joaquin <124814537+Joacs49@users.noreply.github.com>
    Date:   Mon Feb 10 13:19:01 2025 -0500
    
        Merge pull request #1 from yuntasproducciones/pre-main
    
        Se realizo modificaciones en el modelo y migracion de user

---
+++ app/Http/Controllers/Api/V1/Category/CategoriasController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Category;
+
+use App\Http\Controllers\Controller;
+use App\Models\Categorias;
+use Illuminate\Http\Request;
+use App\Http\Requests\PostCategory\PostCategorias;
+
+class CategoriasController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store(PostCategorias $request)
+    {
+        
+    }
+
+ 
+    public function show(Categorias $categorias)
+    {
+        
+    }
+
+    public function edit(Categorias $categorias)
+    {
+        
+    }
+
+    public function update(PostCategorias $request, Categorias $categorias)
+    {
+        
+    }
+
+
+    public function destroy(Categorias $categorias)
+    {
+        
+    }
+}

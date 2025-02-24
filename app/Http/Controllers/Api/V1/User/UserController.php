e136e45932129e99e9dbe207cc8309a374a3c354
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2025-02-24 14:56:20 -0500

    Implementar el trait HttpResponseHelper y mejorar el manejo de respuestas en UserController

--- app/Http/Controllers/Api/V1/User/UserController.php
+++ app/Http/Controllers/Api/V1/User/UserController.php
@@ -9,49 +9,50 @@ use App\Http\Requests\PostUser\PostUser;
 use App\Http\Requests\PostUser\PostUserUpdate;
 use App\Traits\HttpResponseHelper;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
+    use HttpResponseHelper;
     public function register(PostUser $request)
+
     {
-        try{
-            $userData = $request->all();
+        try {
+            DB::beginTransaction();
+            
+            $userData = $request->validated();
             $userData['password'] = Hash::make($request['password']);
 
             $user = User::create($userData);
-    
             $user->assignRole('USER');
-    
-            // return HttpResponseHelper::make()
-            //     ->successfulResponse('Usuario creado correctamente')
-            //     ->send();
-    
+            
+            DB::commit();
+
+            return $this->successfulResponse('Usuario creado correctamente', $user)->send();
+
         } catch (\Exception $e) {
-            // return HttpResponseHelper::make()
-            //     ->internalErrorResponse('Ocurrio un problema al procesar la solicitud. ' . $e->getMessage())
-            //     ->send();
+            DB::rollBack();
+            return $this->internalErrorResponse('Error al crear usuario: ' . $e->getMessage())->send();
         }
     }
 
     public function listUsers()
     {
         try {
-            $userList = User::select('users.user_id','users.name','users.apellido','users.email',
-            'users.usuario','roles.name as role')->join('model_has_roles', 
-            'users.user_id', '=', 'model_has_roles.model_id')
-            ->join('roles', 'model_has_roles.role_id',
-             '=', 'roles.id')->get();
+            $userList = User::select('users.user_id', 'users.name', 
+                'users.email', 'users.celular', 'users.fecha', 
+                'roles.name as role')
+                ->join('model_has_roles', 'users.user_id', '=', 'model_has_roles.model_id')
+                ->join('roles', 'model_has_roles.role_id', '=', 'roles.id')
+                ->get();
 
-            //  return HttpResponseHelper::make()
-            //  ->successfulResponse(
-            //      $userList->isEmpty() ? "No hay usuarios disponibles." : "Usuarios listados correctamente.", 
-            //      $userList
-            //  )->send();
+            return $this->successfulResponse(
+                $userList->isEmpty() ? "No hay usuarios disponibles." : "Usuarios listados correctamente.",
+                $userList
+            )->send();
 
         } catch(\Exception $e) {
-            // return HttpResponseHelper::make()
-            // ->internalErrorResponse("Ocurrio un problema al listar los usuarios." . $e->getMessage())
-            // ->send();
+            return $this->internalErrorResponse("Ocurrió un problema al listar los usuarios." . $e->getMessage())->send();
         }
     }
 
@@ -61,35 +62,26 @@ class UserController extends Controller
             $userId = User::findOrFail($user);
             $userId->delete();
 
-            // return HttpResponseHelper::make()
-            // ->successfulResponse('Usuario eliminado.')
-            // ->send();
+            return $this->successfulResponse('Usuario eliminado.')->send();
 
         } catch(\Exception $e) {
-            // return HttpResponseHelper::make()
-            // ->internalErrorResponse("Ocurrio un problema con la eliminacion del usuario." . $e->getMessage())
-            // ->send();
+            return $this->internalErrorResponse("Ocurrió un problema con la eliminación del usuario." . $e->getMessage())->send();
         }
     }
 
     public function updateUser(PostUserUpdate $request, User $user)
     {
         try {
-            $user->update($request->all());
+            $user->update($request->validated());
 
-            // return HttpResponseHelper::make()
-            //     ->successfulResponse(
-            //         $user->wasChanged() 
-            //             ? "Usuario actualizado correctamente." 
-            //             : "No hubo cambios en los datos del usuario."
-            //     )
-            //     ->send();
+            return $this->successfulResponse(
+                $user->wasChanged() 
+                    ? "Usuario actualizado correctamente." 
+                    : "No hubo cambios en los datos del usuario."
+            )->send();
 
         } catch (\Exception $e) {
-            // return HttpResponseHelper::make()
-            //     ->internalErrorResponse("Ocurrió un problema al actualizar al usuario. " . $e->getMessage())
-            //     ->send();
+            return $this->internalErrorResponse("Ocurrió un problema al actualizar al usuario. " . $e->getMessage())->send();
         }
     }
-
 }
